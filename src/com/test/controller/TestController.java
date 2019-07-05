2f33f1dabf1a7b55a80a8c5c0da4c8f7443dd5b1
LY <448496979@qq.com>
2019-07-05 15:09:00 +0800

    ly

--- src/com/test/controller/TestController.java
+++ src/com/test/controller/TestController.java
@@ -1,8 +1,23 @@
 package com.test.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
+	
+	//登录指令
+		@ResponseBody
+		@RequestMapping(value="/login.do",produces="html/text;charset=utf-8")
+		public String login(String username,String password,String status,HttpServletRequest request,HttpServletResponse reponse,HttpSession session) {
+			System.out.println(username+"++++"+password+"+++"+status);
+			session.setAttribute("username",username);
+			return "1";
+		}
 	
 }
